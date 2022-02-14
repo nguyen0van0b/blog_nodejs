69214789aba6fef9aacd7abc79ceba8e9075e822
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-02-14 16:10:16 +0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
