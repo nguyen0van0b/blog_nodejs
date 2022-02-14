277560d3e00e7f7952dfa70fd9357da2c11ea82f
nguyen0van0b <44321049+nguyen0van0b@users.noreply.github.com>
2022-02-14 15:55:26 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+    })
+
+app.get('/123', (req, res) => {
+    var a = 2,b=3;
+    var c = a + b;
+    var d = a - b;
+    res.send('Hello World! c = ' + c + " - d = " + d )
+  })
+
+app.get('/phuoc', (req, res) => {
+    res.send('Hello Phuoc')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
